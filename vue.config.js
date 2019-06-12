e7b9bed5b3cc30e8841668e3cdb7284bb899c1bf
yc <3095220780@qq.com>
2019-06-12 15:55:07 +0800

    sadsa

---
+++ vue.config.js
@@ -0,0 +1,41 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
+const isProduction = process.env.NODE_ENV === 'production';
+// const path = require('path')
+
+// function resolve(dir){
+//     return path.join(__dirname,'./',dir)
+// }
+
+// const externals = {
+//     'vue':'Vue',
+//     ''
+// }
+
+module.exports = {
+    lintOnSave: true,
+    productionSourceMap:false,
+    devServer: {
+        host: 'localhost',
+        https: false, // https:{type:Boolean}
+        proxy: {
+            '/api': {
+                target: 'http://39.106.80.90:3000/',
+                changeOrigin: true,
+                pathRewrite:{
+                    '^/api':''
+                }
+            }
+        }
+    },
+    configureWebpack: config => {
+        if (isProduction) {
+          config.plugins.push(new CompressionWebpackPlugin({
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            threshold: 10240,
+            minRatio: 0.8
+          }))
+        }
+    }
+}
